277ef8acd3f3c8794472903bb56fa98ca708f5f5
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2023-10-25 23:43:00 +0300

    first build

--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using static Unity.VisualScripting.Metadata;
 
 public class TileSpawner : MonoBehaviour
 {
